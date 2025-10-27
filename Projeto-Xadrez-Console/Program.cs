ceb439a64e0081e03d22d78dbbf8fd9c8a838d24
uniedit <buraconegroquasar@gmail.com>
2025-10-27 18:28:02 -0300

    Criação do Projeto

---
+++ Projeto-Xadrez-Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_Xadrez_Console {
+    internal class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
