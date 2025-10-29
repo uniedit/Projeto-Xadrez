3fda0ff41dc13a69ea582b5fcc3bc8a6683866f8
uniedit <buraconegroquasar@gmail.com>
2025-10-29 19:34:11 -0300

    Melhoria do método ImprimirTabuleiro e delegação de impressão de cor

--- Projeto-Xadrez-Console/Program.cs
+++ Projeto-Xadrez-Console/Program.cs
@@ -7,9 +7,20 @@ namespace Projeto_Xadrez_Console {
     internal class Program {
         static void Main(string[] args) {
 
-            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
-            Console.WriteLine(pos);
-            Console.WriteLine(pos.ToPosicao());
+            try {
+
+                Tabuleiro tab = new Tabuleiro(8, 8);
+
+                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+                tab.ColocarPeca(new Torre(tab, Cor.Branca), new Posicao(3, 5));
+
+                Tela.ImprimirTabuleiro(tab);
+
+            } catch (TabuleiroException e) {
+                Console.WriteLine(e.Message);
+            }
 
         }
     }
--- Projeto-Xadrez-Console/Tela.cs
+++ Projeto-Xadrez-Console/Tela.cs
@@ -5,19 +5,49 @@ namespace Projeto_Xadrez_Console {
     class Tela {
 
         public static void ImprimirTabuleiro(Tabuleiro tab) {
+            Console.WriteLine("    A B C D E F G H");
+            CorConsole("   -----------------");
+            Console.WriteLine();
 
             for (int l = 0; l < tab.Linhas; l++) {
+                Console.Write(8 - l);
+                CorConsole(" | ");
                 for (int c = 0; c < tab.Colunas; c++) {
                     if (tab.Peca(l, c) == null) {
-                        Console.Write("- ");
+                        if (c == 7) {
+                            Console.Write("-");
+                        } else {
+                            Console.Write("- ");
+                        }
                     } else {
-                        Console.Write(tab.Peca(l, c) + " ");
+                        ImprimirPeca(tab.Peca(l, c));
                     }
 
                 }
+                CorConsole(" | ");
+                Console.Write(8 - l);
                 Console.WriteLine();
             }
+            CorConsole("   -----------------");
+            Console.WriteLine("");
+            Console.WriteLine("    A B C D E F G H");
+        }
 
+        public static void ImprimirPeca(Peca peca) {
+            if (peca.Cor == Cor.Branca) {
+                Console.Write(peca + " ");
+            } else {
+                CorConsole($"{peca} ");
+            }
+        }
+
+        private static ConsoleColor Aux;
+
+        public static void CorConsole(string message) {
+            Aux = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write(message);
+            Console.ForegroundColor = Aux;
         }
 
     }
