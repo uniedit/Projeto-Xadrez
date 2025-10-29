105df1d53a09312f0bf06fb54bd64eaffc4a3948
uniedit <buraconegroquasar@gmail.com>
2025-10-29 18:21:35 -0300

    Criação da classe PosicaoXadrez

--- Projeto-Xadrez-Console/Program.cs
+++ Projeto-Xadrez-Console/Program.cs
@@ -7,19 +7,9 @@ namespace Projeto_Xadrez_Console {
     internal class Program {
         static void Main(string[] args) {
 
-            try {
-                Tabuleiro tab = new Tabuleiro(8, 8);
-
-                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, -1));
-                //tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 0));
-                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-                tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
-
-                Tela.ImprimirTabuleiro(tab);
-            } catch (TabuleiroException e) {
-                Console.WriteLine(e.Message);
-            }
+            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
+            Console.WriteLine(pos);
+            Console.WriteLine(pos.ToPosicao());
 
         }
     }
---
+++ Projeto-Xadrez-Console/Xadrez/PosicaoXadrez.cs
@@ -0,0 +1,24 @@
+﻿using Projeto_Tabuleiro;
+
+namespace Projeto_Xadrez {
+    class PosicaoXadrez {
+
+        public char Coluna { get; set; }
+        public int Linha { get; set; }
+
+        public PosicaoXadrez(char coluna, int linha) {
+            this.Coluna = coluna;
+            this.Linha = linha;
+        }
+
+        public Posicao ToPosicao() {
+            // Internamente 'a' é um numero inteiro, assim faz o calculo continuo com o resto do alfabeto
+            return new Posicao(8 - Linha, Coluna - 'a');
+        }
+
+        public override string ToString() {
+            return $"{Coluna}{Linha}";
+        }
+
+    }
+}
